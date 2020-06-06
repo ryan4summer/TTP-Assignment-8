1515f753de06cb42bc9d9cba03774b741ad4ebbd
Jan Markus Milan <jmarkusm@me.com>
2020-06-06 16:55:45 -0400

    Added some styling (a header and some css)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from "./components/layout/Header";
 import Table from "./components/Table";
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container">
+          <Header />
           <Table />
         </div>
       </div>
